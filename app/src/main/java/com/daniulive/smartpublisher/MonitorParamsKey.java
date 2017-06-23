8b6c58b26a85f527f537ad8b2919987db3a6ad5f
120585 <120585@nd.com>
2017-06-23 21:06:34 +0800

    后台推送的example

---
+++ app/src/main/java/com/daniulive/smartpublisher/MonitorParamsKey.java
@@ -0,0 +1,26 @@
+//package com.daniulive.smartpublisher;
+//
+///**
+// * Created by leo on 17/6/23.
+// */
+//
+//public class MonitorParamsKey {
+//    // 推送类型
+//    public final static String pushType = "PUSHTYPE";
+//    // 视频宽
+//    public final static String camerawidth = "CAMERAWIDTH";
+//    // 视频高
+//    public final static String cameraheight = "CAMERAHEIGHT";
+//    // 截屏分辨率
+//    public final static String screenresolution = "SCREENRESOLUTION";
+//    // 前后摄像头
+//    public final static String switchcamera = "SWITCHCAMERA";
+//    // 推送地址
+//    public final static String publishurl = "PUBLISHURL";
+//    // 是否本地保存录制
+//    public final static String recorder = "RECORDER";
+//    // 是否硬解码
+//    public final static String hwencoder = "HWENCODER";
+//    // 录制音屏地址
+//    public final static String recoderDir = "RecoderDir";
+//}
